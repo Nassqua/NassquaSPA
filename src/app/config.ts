db819f489d4c1ad225bd54ef42334bf495bb6327
Heinner Zapata Escarraga <heinner.zapata@globant.com>
2017-08-19 20:42:46 -0500

    redux issues

--- src/app/config.ts
+++ src/app/config.ts
@@ -3,8 +3,8 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class Config{
 
-  //static apiProvider : string = 'http://localhost:3000/api/'; // Localhost
-  static apiProvider : string = 'https://nassqua-api-node.azurewebsites.net/api/'; // Azure
+  static apiProvider : string = 'http://localhost:3000/api/'; // Localhost
+  //static apiProvider : string = 'https://nassqua-api-node.azurewebsites.net/api/'; // Azure
 
   contructor(){}
 
